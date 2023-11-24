8a5cbfd38a4e9f7ce2fa2c61ad39c5d8af3b6950
Claydes <makc3369@gmail.com>
2023-11-24 18:49:55 +0200

    add requirements.txt

--- main.py
+++ main.py
@@ -5,31 +5,41 @@ import lwa
 import numpy as np
 
 
-model = words_model.words_6
+def process(grades, grades_model, res_model):
+    W = []
+    for item in grades_model['words']:
+        W.append(grades.count(item))
 
-grades = [list(model['words'].keys())[random.randrange(0, 1)] for _ in range(25)] + \
-         [list(model['words'].keys())[random.randrange(5, 6)] for _ in range(25)]
-W = []
-for item in model['words']:
-    W.append(grades.count(item))
+    h = min(item['lmf'][-1] for item in grades_model['words'].values())
+    m = 50
+    intervals_umf = lwa.alpha_cuts_intervals(m)
+    intervals_lmf = lwa.alpha_cuts_intervals(m, h)
 
-h = min(item['lmf'][-1] for item in model['words'].values())
-m = 50
-intervals_umf = lwa.alpha_cuts_intervals(m)
-intervals_lmf = lwa.alpha_cuts_intervals(m, h)
+    res_lmf = lwa.y_lmf(intervals_lmf, grades_model, W)
+    res_umf = lwa.y_umf(intervals_umf, grades_model, W)
+    res = lwa.construct_dit2fs(np.arange(*grades_model['x']), intervals_lmf, res_lmf, intervals_umf, res_umf)
+
+    # res.plot()
+
+    sm = []
+    for title, fou in res_model['words'].items():
+        sm.append((title, res.similarity_measure(word.Word(None, res_model['x'], fou['lmf'], fou['umf']))))
+    res_word = max(sm, key=lambda item: item[1])
+    return res_word
 
 
-res_lmf = lwa.y_lmf(intervals_lmf, model, W)
-res_umf = lwa.y_umf(intervals_umf, model, W)
-res = lwa.construct_dit2fs(np.arange(*model['x']), intervals_lmf, res_lmf, intervals_umf, res_umf)
-res.plot()
+def generate_results(grades_count, tests_num):
+    grades_model = words_model.words_7
+    res_model = words_model.words_11
+    for i in range(tests_num):
+        grades = [list(grades_model['words'].keys())[random.randrange(
+            0, len(grades_model['words']))] for _ in range(grades_count)]
+        print(f"Grades: {grades}")
+        print(f"Result: {process(grades, grades_model, res_model)}\n")
 
-sm = []
-model = words_model.words_32
-for title, fou in model['words'].items():
-    sm.append((title, res.similarity_measure(word.Word(None, model['x'], fou['lmf'], fou['umf']))))
-res_word = max(sm, key=lambda item: item[1])
-print(res_word)
+
+if __name__ == '__main__':
+    generate_results(10, 5)
 
 
 
